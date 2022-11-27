5b723c3a7d10f6895d8ae333b6b859266842b9f8
Thang <vietnamgoldfruit@gmail.com>
2022-11-27 21:53:31 +0700

    Thang push: add mau attack

--- ThangGame3D/Assets/Scrips/Player.cs
+++ ThangGame3D/Assets/Scrips/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
     
     private Rigidbody rb;
     public List<Transform> waypoints;
-    public float moveSpeed = 2f;
+    private Transform lastPosition => waypoints[waypoints.Count - 1];
+    public float moveSpeed = 4f;
     Vector3 targetPosition;
     int waypointIndex = 0;
     int index = 1;
@@ -16,10 +17,16 @@ public class Player : MonoBehaviour
     public int maxHealth = 100;
     public int curHealth;
     public int damage = 10;
+    public float attackSpeech;
+    float countAttackTime;
+    bool attack = false;
     public HealthBar healthBar;
+    public Transform targetTransform;
+    public Tower tower;
     void Start()
     {
-        rb= this.GetComponent<Rigidbody>();
+        countAttackTime= attackSpeech;
+        rb = this.GetComponent<Rigidbody>();
         transform.position = waypoints[waypointIndex].position;
         UpdateTransform();
         anima = gameObject.GetComponent<Animator>();
@@ -29,38 +36,70 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-        Vector3 direction = transform.position - targetPosition;
 
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //Quaternion quaternion = Quaternion.AngleAxis(angle, Vector3.forward);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, 1 * Time.deltaTime);
+    {
+        countAttackTime -= Time.deltaTime;
+        if (countAttackTime <= 0 && attack)
+        {
+            Attack();
+
+        }
+        if (tower.curHealth<=0 || tower.gameObject==null) 
+        {
+            anima.SetBool("attack", false);
+            attack = false;
+            Destroy(tower.gameObject);
+        }
+        if (curHealth<=0) 
+        {
+            //anima.SetBool("Die", true);
+            anima.SetTrigger("Die 0");
+            anima.SetBool("attack", false);
+            attack= false;
+        }
+
+        if (lastPosition == targetTransform)
+        {
+            var distance = Vector3.Distance(targetPosition, transform.position);
+            if (distance <= 1.5)
+                return;
+        }
+        Vector3 direction = targetPosition - transform.position;
+       transform.rotation = Quaternion.LookRotation(direction);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         float dist = Vector3.Distance(targetPosition, transform.position);
         if (dist <= 0.4f) { UpdateTransform(); }
-        if (Input.GetKeyDown(KeyCode.Space)) { TakeDamage(20); }
 
     }
     private void UpdateTransform()
     {
-        
-            waypointIndex += index;
-            targetPosition = waypoints[waypointIndex].position;
-        
-        
+        waypointIndex += index;
+        if (waypointIndex > waypoints.Count)
+            return;
+        targetTransform = waypoints[waypointIndex];
+        targetPosition = targetTransform.position;
+       
     }
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag =="Tower")
+        if (other.gameObject.tag =="Tower" && tower.curHealth>0)
         {
             anima.SetBool("attack", true);
-           
-        }
+            attack = true;
+        } 
 
     }
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         curHealth-=damage;
         healthBar.SetHealth(curHealth);
+    }    
+    void Attack()
+    {
+        countAttackTime = attackSpeech;
+        if (tower.curHealth>0)
+        {
+            tower.TakeDamage(damage);
+        }    
     }    
 }
--- ThangGame3D/Assets/Scrips/Tower.cs
+++ ThangGame3D/Assets/Scrips/Tower.cs
@@ -7,33 +7,59 @@ public class Tower : MonoBehaviour
     private Rigidbody rb;
     public int maxHealth = 100;
     public int curHealth;
-    public int damage = 10;
+    public int damage = 20;
+    public float attackSpeech = 1f;
+    public float countAttackTime;
     public HealthBar healthBar;
-    Player player;
-    bool isAttacked= false;
+    public Player player;
+    bool Attack= false;
+    public Animator anima;
     void Start()
     {
-        player= GetComponent<Player>();
+        //player = FindObjectOfType<Player>();
         rb = this.GetComponent<Rigidbody>();
         curHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        anima = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (isAttacked == true) { TakeDamage(player.damage); }
+        countAttackTime -= Time.deltaTime;
+        if (countAttackTime <= 0 && Attack)
+        {
+            
+            towerAttack();
+
+        }
+        if (player.curHealth <= 0)
+        {
+           // player.anima.SetBool("attack", false);
+            Attack = false;
+           // return;
+            
+        }
     }
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         curHealth -= damage;
         healthBar.SetHealth(curHealth);
     }
-    private void OnTriggerStay(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "sword")
+        if (other.gameObject.tag == "Player" && player.curHealth > 0)
         {
-            isAttacked= true;
-
+            Attack = true;
+            player.anima.SetBool("Gethit", true);
+        }
+    }
+  
+    void towerAttack()
+    {
+        countAttackTime = attackSpeech;
+        if (player.curHealth > 0)
+        {
+            player.TakeDamage(damage);
         }
     }
 }
